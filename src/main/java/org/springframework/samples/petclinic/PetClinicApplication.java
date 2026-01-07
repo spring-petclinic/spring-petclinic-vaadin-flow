2c5a63ea87d1ecfd1bc3e226af003947780f435b
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2026-01-07 14:36:47 +0200

    fix import order

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -16,7 +16,6 @@
 
 package org.springframework.samples.petclinic;
 
-import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -24,6 +23,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.page.AppShellConfigurator;
 import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.lumo.Lumo;
 
 /**
  * PetClinic Spring Boot Application.
